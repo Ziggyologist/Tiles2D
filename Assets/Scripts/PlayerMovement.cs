ea6ed0f2045f327a2794c61961c66a57c2b0e480
Teodora Vela <teodora11vela@gmail.com>
2025-01-29 12:26:02 +0200

    implement jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,15 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
-    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float runSpeed = 6f;
+    [SerializeField] float jumpSpeed = 20f;
     Rigidbody2D rb;
+    Animator animator;
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
     }
 
     void Update()
@@ -24,10 +28,19 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log(moveInput);
     }
 
+    void OnJump(InputValue value) 
+    {
+        if (value.isPressed) 
+        {
+            rb.linearVelocity += new Vector2(0f, jumpSpeed);
+        }
+    }
     void Run()
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb.linearVelocity.y);
         rb.linearVelocity = playerVelocity;
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.linearVelocity.x) > Mathf.Epsilon;
+        animator.SetBool("isRunning", playerHasHorizontalSpeed);
     }
 
     void FlipSprite()
