f8960668cc58bdc731a95e27cdb4238aa5e84a98
Teodora Vela <teodora11vela@gmail.com>
2025-02-02 18:25:10 +0200

    added hazards

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -91,7 +91,7 @@ public class PlayerMovement : MonoBehaviour
     void Die()
     {
 
-        if (bodyCollider2d.IsTouchingLayers(LayerMask.GetMask("Enemies")))
+        if (bodyCollider2d.IsTouchingLayers(LayerMask.GetMask("Enemies", "Hazards")))
         {
             isAlive = false;
             animator.SetTrigger("Dying");
