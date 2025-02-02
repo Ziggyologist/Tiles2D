8ad77487d84807650bc213f6ddc90a1b3ffa536d
Teodora Vela <teodora11vela@gmail.com>
2025-02-02 16:31:14 +0200

    added enemy collision and movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D rb;
+    BoxCollider2D boxCollider;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(moveSpeed, 0f);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("exit collision");
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+
+    void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(rb.linearVelocity.x)), 1f);
+        //transform.localScale = new Vector2(-Mathf.Sign(-moveSpeed), 1);
+
+    }
+}
